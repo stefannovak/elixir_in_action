e959c582b3db23def4e7dda66567a583ceb243cf
Stefan Novak <stefannovak96@gmail.com>
2024-06-15 23:24:34 +0100

    feat: range

--- lib/elixir_in_action.ex
+++ lib/elixir_in_action.ex
@@ -17,7 +17,7 @@ defmodule ChapterThree do
       iex> ChapterThree.list_len([1,2,3,4,5,6])
       6
   """
-  def list_len(list) do
+  def list_len(list) when is_list(list) do
     list_len_acc(0, list)
   end
 
@@ -27,5 +27,28 @@ defmodule ChapterThree do
 
   defp list_len_acc(length, [_ | tail]) do
     list_len_acc(length + 1, tail)
+  end
+
+  @doc """
+  Calculate the range between two numbers.
+  a must be lower than b, and must be numbers.
+  The range returned is reversed.
+
+  ## Examples
+
+    iex> ChapterThree.range(1, 3)
+    [3, 2, 1]
+  """
+  @spec range(number(), number()) :: [number(), ...]
+  def range(a, b) when is_number(a) and is_number(b) and a < b do
+    range_acc(b, [a])
+  end
+
+  defp range_acc(last, accumulator) do
+    if hd(accumulator) + 1 == last do
+      [last | accumulator]
+    else
+      range_acc(last, [hd(accumulator) + 1 | accumulator])
+    end
   end
 end
--- test/elixir_in_action_test.exs
+++ test/elixir_in_action_test.exs
@@ -2,7 +2,19 @@ defmodule ChapterThreeTest do
   use ExUnit.Case
   doctest ChapterThree
 
+  # list_len
+
   test "calculates correct length" do
     assert ChapterThree.list_len([1, 5, 3]) == 3
+  end
+
+  assert_raise FunctionClauseError, fn ->
+    ChapterThree.list_len(5)
+  end
+
+  # range
+
+  test "calculates correct range" do
+    assert ChapterThree.range(1, 5) == [5, 4, 3, 2, 1]
   end
 end
