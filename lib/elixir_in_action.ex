aa7ada8ac2f16c09d3cbcd55f2515b7930caad38
Stefan Novak <stefannovak96@gmail.com>
2024-06-15 23:34:10 +0100

    feat: positive/1

--- lib/elixir_in_action.ex
+++ lib/elixir_in_action.ex
@@ -51,4 +51,29 @@ defmodule ChapterThree do
       range_acc(last, [hd(accumulator) + 1 | accumulator])
     end
   end
+
+  @doc """
+  Returns the reverse ordered, positive elements of a given integer list.
+
+  ## Examples
+
+    iex> ChapterThree.positive([1, 2, 6, 3, 8, 10])
+    [10, 8, 6, 2]
+  """
+  @spec positive([integer()]) :: [integer()]
+  def positive(list) when is_list(list) do
+    positive_acc(list, [])
+  end
+
+  defp positive_acc([], acc) do
+    acc
+  end
+
+  defp positive_acc([head | tail], acc) do
+    if rem(head, 2) == 0 do
+      positive_acc(tail, [head | acc])
+    else
+      positive_acc(tail, acc)
+    end
+  end
 end
--- test/elixir_in_action_test.exs
+++ test/elixir_in_action_test.exs
@@ -2,7 +2,7 @@ defmodule ChapterThreeTest do
   use ExUnit.Case
   doctest ChapterThree
 
-  # list_len
+  # list_len/1
 
   test "calculates correct length" do
     assert ChapterThree.list_len([1, 5, 3]) == 3
@@ -12,9 +12,24 @@ defmodule ChapterThreeTest do
     ChapterThree.list_len(5)
   end
 
-  # range
+  # range/2
 
   test "calculates correct range" do
     assert ChapterThree.range(1, 5) == [5, 4, 3, 2, 1]
+  end
+
+  assert_raise FunctionClauseError, fn ->
+    ChapterThree.range(5, 2)
+  end
+
+
+  assert_raise FunctionClauseError, fn ->
+    ChapterThree.range([1], 2)
+  end
+
+  # positive/1
+
+  test "calculates correct list" do
+    assert ChapterThree.positive([1, 2, 3, 4, 5]) == [4, 2]
   end
 end
